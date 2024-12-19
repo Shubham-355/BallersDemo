922f93917c887f3a24c1cc6878d370a5a8a9e468
Shubham-355 <smsuthar2520@gmail.com>
2024-12-19 23:17:32 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import PhilanthropyPage from './pages/PhilanthropyPage';
 import InvestmentsPage from './Pages/InvestmentsPage';
 import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 
 
 function App() {
@@ -13,14 +13,12 @@ function App() {
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <main className="flex-grow">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/philanthropy" element={<PhilanthropyPage />} />
             <Route path="/investments" element={<InvestmentsPage />} />
           </Routes>
-        </main>
         <Footer />
       </div>
     </Router>
